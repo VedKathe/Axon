7a2662a6fbcf9c029494a75ce6d08271632960c1
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2024-05-30 02:14:01 +0530

    Update dashboard.jsx

--- Axon/src/pages/dashboard.jsx
+++ Axon/src/pages/dashboard.jsx
@@ -4,7 +4,7 @@ import Ask from '../component/ask'
 
 export default function Dashboard() {
 
-    const [selectedOption, setSelectedOption] = useState('analyze')
+    const [selectedOption, setSelectedOption] = useState('other')
 
     const handleOptionClick = (option) => {
       setSelectedOption(option)
@@ -14,8 +14,8 @@ export default function Dashboard() {
     <>
     <div style={{height: '100vh', width: '100vw', overflowY: 'hidden', display: 'flex'}}>
         <div className='left_div' style={{height: '100%', backgroundColor: 'grey', display: 'flex', flex: '.2', flexDirection: 'column'}}>
-            <div className='tab'  onClick={() => handleOptionClick('analyze')}>Analyze</div>
             <div className='tab' style={{backgroundColor: 'blue', width: '100%', height: 'max-content'}} onClick={() => handleOptionClick('other')}>AXON</div>
+            <div className='tab'  onClick={() => handleOptionClick('analyze')}>Analyze</div>
         </div>
 
         <div className='right_div' style={{height: '100%', backgroundColor: 'white', display: 'flex', flex: '1', overflowY: 'scroll'}}>
